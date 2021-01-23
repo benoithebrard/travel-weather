3a15f8c742253a634181dd1b4795d5d3f875c05a
Benoit Hebrard <benoit@shape.dk>
2021-01-23 17:28:11 +0100

    complete listener

--- app/src/main/java/com/bempaaa/travelweather/ui/destination/DestinationPickerFragment.kt
+++ app/src/main/java/com/bempaaa/travelweather/ui/destination/DestinationPickerFragment.kt
@@ -2,6 +2,8 @@ package com.bempaaa.travelweather.ui.destination
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
 import com.bempaaa.travelweather.NavigationListener
 import com.bempaaa.travelweather.R
@@ -15,5 +17,22 @@ class DestinationPickerFragment : Fragment(R.layout.fragment_destination_picker)
         toolbar.setNavigationOnClickListener {
             (requireActivity() as? NavigationListener)?.onBackClicked()
         }
+
+        motion_layout.setTransitionListener(object : MotionLayout.TransitionListener {
+            override fun onTransitionStarted(p0: MotionLayout, p1: Int, p2: Int) {
+            }
+
+            override fun onTransitionChange(p0: MotionLayout, p1: Int, p2: Int, p3: Float) {
+            }
+
+            override fun onTransitionCompleted(p0: MotionLayout, p1: Int) {
+                if (p0.progress == 1f) {
+                    Toast.makeText(requireContext(), "Plane has landed", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onTransitionTrigger(p0: MotionLayout, p1: Int, p2: Boolean, p3: Float) {
+            }
+        })
     }
 }
